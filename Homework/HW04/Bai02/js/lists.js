75951d1ff0cb887493ed7c64937cbf3b5e8b1f44
Dao Nhat Thanh [PG] <nhat_thanh@lampart-vn.com>
2018-07-26 13:28:25 +0700

    done Bai02 HW04

---
+++ Homework/HW04/Bai02/js/lists.js
@@ -0,0 +1,33 @@
+// Declaration variables
+let name = document.getElementById('name');
+let btn = document.getElementById('add');
+let parent =document.getElementById('list-name');
+let select = parent.querySelectorAll("option");
+//console.log(select[2].value);
+
+// Add Event
+btn.addEventListener('click',function() {
+    let names = name.value;
+    // check empty field
+    if(names.trim() ==="") {
+        alert("Bạn phải nhập họ tên");
+        return false;
+    }
+
+    // check same field
+    for(let i in select) {
+        if(names === select[i].value) {
+            alert("Họ tên bị trùng");
+            return false;
+        }
+    }
+    // create Element child
+    let newItems = document.createElement("OPTION");
+    let textNode = document.createTextNode(names);
+    newItems.appendChild(textNode);
+    newItems.value = names;
+
+    // insert into Element parent
+    parent.insertBefore(newItems, parent.childNodes[0]);
+});
+
--- Lab/Lab04/Bai02/js/valid.js
+++ Lab/Lab04/Bai02/js/valid.js
@@ -1,27 +1,16 @@
 $(function() {
-
     let fullname = $('#fname');
-
     let username = $('#username');
-
-    let password = $('#password');
-
-    let password_retype = $('#password_retype');
-
     let day = $('#days');
     let month = $('#months');
     let year = $('#years');
-
     let male = $("#male");
     let female = $("#female");
 
-    function checkForm() {
-        if(fullname.val() ==="" || username.val() === "") {
-            console.log("required");
-            return false;
-        }
+    let password = $('#password');
+    let password_retype = $('#password_retype');
 
-
+    function checkPass(password) {
         let pass = password.val();
         if(pass.length <6) {
             console.log("min length 6");
@@ -29,11 +18,27 @@ $(function() {
             return false;
         } else {
             console.log("OKE PASS");
-            let stringPass = "1234567890!@#$%^&*()qwertyuiopasdfghjklzxcvbnm";
-            let n = stringPass.includes(pass); //TODO: end time
+            let stringRequire = "ZXCVBNMASDFGHJKLQWERTYUIOPqwertyuiopasdfghjklzxcvbnm";
+            let numberRequire = "1234567809";
+            let specialChar = "$*&!";
+            //let n = stringPass.includes(pass); //TODO: end time
+
+
+            let str_require = stringRequire.split();
+            for(let i in str_require) {
+                let str_includes = str_require[i].includes(pass);
+                console.log(str_includes);
+            }
+
+
+
+            function checkOneCharExist() {
+
+            }
+
+            return false;
 
         }
-
         /**
          * function myFunction() {
                 var str = "Hello world, welcome to the universe.";
@@ -42,7 +47,19 @@ $(function() {
             }
          */
 
-    }
 
+
+    }
+    function checkForm() {
+        if(fullname.val() ==="" || username.val() === "") {
+            console.log("required");
+            return false;
+        }
+        if(checkPass(password) !== true) {
+            return false;
+        }
+
+
+    }
     $("#main-form").on('submit',checkForm);
 });
